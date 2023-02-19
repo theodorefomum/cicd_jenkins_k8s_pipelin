f9033a5bb4f74fd3502531d9552adab80ca0411c
Maxcell Ayim <maxcellayim588@gmail.com>
2023-02-18 21:34:24 -0500

    updating files

--- Terraform/eks-cluster.tf
+++ Terraform/eks-cluster.tf
@@ -1,41 +1,99 @@
 resource "aws_eks_cluster" "max_cluster" {
   name     = "max_prod_cluster"
-  role_arn = aws_iam_role.max_iam_role.arn
+  role_arn = aws_iam_role.eks-iam-role.arn
 
   vpc_config {
     subnet_ids = [aws_subnet.max_sbn-pub.id, aws_subnet.max_sbn-pub1.id]
   }
+  depends_on = [
+    aws_iam_role.eks-iam-role,
+ ]
 }
 
-resource "aws_iam_role" "max_iam_role" {
-  name = "max_prod_cluster_role"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
+resource "aws_iam_role" "workernodes" {
+  name = "eks-node-group-example"
+ 
+  assume_role_policy = jsonencode({
+   Statement = [{
+    Action = "sts:AssumeRole"
+    Effect = "Allow"
+    Principal = {
+     Service = "ec2.amazonaws.com"
     }
+   }]
+   Version = "2012-10-17"
+  })
+ }
+
+  resource "aws_eks_node_group" "worker-node-group" {
+  cluster_name  = aws_eks_cluster.max_cluster.name
+  node_group_name = "devopsthehardway-workernodes"
+  node_role_arn  = aws_iam_role.workernodes.arn
+  subnet_ids   = [aws_subnet.max_sbn-pub.id, aws_subnet.max_sbn-pub1.id]
+  instance_types  = ["t2.micro"]
+ 
+  scaling_config {
+   desired_size = 1
+   max_size   = 1
+   min_size   = 1
+  }
+ 
+  depends_on = [
+   aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+   aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+   #aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role    = aws_iam_role.workernodes.name
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role    = aws_iam_role.workernodes.name
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+  role    = aws_iam_role.workernodes.name
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role    = aws_iam_role.workernodes.name
+ }
+
+resource "aws_iam_role" "eks-iam-role" {
+ name = "devopsthehardway-eks-iam-role"
+
+ path = "/"
+
+ assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+  {
+   "Effect": "Allow",
+   "Principal": {
+    "Service": "eks.amazonaws.com"
+   },
+   "Action": "sts:AssumeRole"
+  }
+ ]
 }
-POLICY
+EOF
 }
 
-resource "aws_iam_role_policy_attachment" "amazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.max_iam_role.name
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+ role    = aws_iam_role.eks-iam-role.name
 }
-
-resource "aws_iam_role_policy_attachment" "amazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/amazonEKSVPCResourceController"
-  role       = aws_iam_role.max_iam_role.name
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+ role    = aws_iam_role.eks-iam-role.name
 }
-
 
 resource "aws_vpc" "max_vpc" {
   cidr_block       = "10.0.0.0/16"
@@ -128,57 +186,4 @@ resource "aws_security_group" "allow_http" {
   tags = {
     Name = "allow_http"
   }
-}
-
-resource "aws_eks_node_group" "max-node" {
-  cluster_name    = aws_eks_cluster.max_cluster.name
-  node_group_name = "max-node"
-  node_role_arn   = aws_iam_role.max_iam_role_node.arn
-  subnet_ids      = aws_subnet.max_sbn-pub[*].id
-  instance_types  = ["t2.micro"]
-
-  scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
-  }
-  depends_on = [
-    aws_iam_role_policy_attachment.amazonEKSNodePolicy,
-    aws_iam_role_policy_attachment.amazonEKSCNIPolicy,
-    aws_iam_role_policy_attachment.amazonEKSec2Policy
-  ]
-}
-
-resource "aws_iam_role" "max_iam_role_node" {
-  name = "max_prod_node_role"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "amazonEKSCNIPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.max_iam_role_node.name
-}
-
-resource "aws_iam_role_policy_attachment" "amazonEKSNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.max_iam_role_node.name
-}
-
-resource "aws_iam_role_policy_attachment" "amazonEKSec2Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
-  role       = aws_iam_role.max_iam_role_node.name
 }
