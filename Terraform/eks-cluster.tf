d75a7348d8738223bac1903a3927f4fe3dd46572
Maxcell Ayim <maxcellayim588@gmail.com>
2023-02-19 00:53:24 -0500

    udates

--- Terraform/eks-cluster.tf
+++ Terraform/eks-cluster.tf
@@ -7,70 +7,70 @@ resource "aws_eks_cluster" "max_cluster" {
   }
   depends_on = [
     aws_iam_role.eks-iam-role,
- ]
+  ]
 }
 
 resource "aws_iam_role" "workernodes" {
   name = "eks-node-group-example"
- 
-  assume_role_policy = jsonencode({
-   Statement = [{
-    Action = "sts:AssumeRole"
-    Effect = "Allow"
-    Principal = {
-     Service = "ec2.amazonaws.com"
-    }
-   }]
-   Version = "2012-10-17"
-  })
- }
 
-  resource "aws_eks_node_group" "worker-node-group" {
-  cluster_name  = aws_eks_cluster.max_cluster.name
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_eks_node_group" "worker-node-group" {
+  cluster_name    = aws_eks_cluster.max_cluster.name
   node_group_name = "devopsthehardway-workernodes"
-  node_role_arn  = aws_iam_role.workernodes.arn
-  subnet_ids   = [aws_subnet.max_sbn-pub.id, aws_subnet.max_sbn-pub1.id]
+  node_role_arn   = aws_iam_role.workernodes.arn
+  subnet_ids      = [aws_subnet.max_sbn-pub.id, aws_subnet.max_sbn-pub1.id]
   instance_types  = ["t2.micro"]
- 
+
   scaling_config {
-   desired_size = 1
-   max_size   = 1
-   min_size   = 1
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
   }
- 
+
   depends_on = [
-   aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-   aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-   #aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    #aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
- }
- 
- resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role    = aws_iam_role.workernodes.name
- }
- 
- resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.workernodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role    = aws_iam_role.workernodes.name
- }
- 
- resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+  role       = aws_iam_role.workernodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
   policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
-  role    = aws_iam_role.workernodes.name
- }
- 
- resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.workernodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role    = aws_iam_role.workernodes.name
- }
+  role       = aws_iam_role.workernodes.name
+}
 
 resource "aws_iam_role" "eks-iam-role" {
- name = "devopsthehardway-eks-iam-role"
+  name = "devopsthehardway-eks-iam-role"
 
- path = "/"
+  path = "/"
 
- assume_role_policy = <<EOF
+  assume_role_policy = <<EOF
 {
  "Version": "2012-10-17",
  "Statement": [
@@ -87,12 +87,33 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
- policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
- role    = aws_iam_role.eks-iam-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-iam-role.name
 }
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
- policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
- role    = aws_iam_role.eks-iam-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks-iam-role.name
+}
+
+
+resource "aws_route_table_association" "public_association" {
+  subnet_id     = aws_subnet.max_sbn-pub.id
+  route_table_id = aws_route_table.public_route.id
+}
+
+resource "aws_route_table_association" "public_association1" {
+  subnet_id     = aws_subnet.max_sbn-pub1.id
+  route_table_id = aws_route_table.public_route.id
+}
+
+resource "aws_route_table_association" "private_association" {
+  subnet_id     = aws_subnet.max_sbn-priv1.id
+  route_table_id = aws_route_table.private_route.id
+}
+
+resource "aws_route_table_association" "private_association1" {
+  subnet_id     = aws_subnet.max_sbn-priv1.id
+  route_table_id = aws_route_table.private_route.id
 }
 
 resource "aws_vpc" "max_vpc" {
@@ -110,7 +131,7 @@ resource "aws_subnet" "max_sbn-pub" {
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "Main"
+    Name = "public"
   }
 }
 
@@ -120,7 +141,7 @@ resource "aws_subnet" "max_sbn-pub1" {
   availability_zone = "us-east-1b"
 
   tags = {
-    Name = "Main"
+    Name = "public"
   }
 }
 
@@ -134,16 +155,49 @@ resource "aws_subnet" "max_sbn-priv" {
   }
 }
 
+resource "aws_subnet" "max_sbn-priv1" {
+  vpc_id            = aws_vpc.max_vpc.id
+  cidr_block        = "10.0.67.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "max_sbn-priv"
+  }
+}
+
 resource "aws_internet_gateway" "max-gw" {
   vpc_id = aws_vpc.max_vpc.id
 }
 
-resource "aws_route_table" "example" {
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.max_sbn-priv.id
+}
+
+resource "aws_nat_gateway" "nat_gateway1" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.max_sbn-priv1.id
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_route_table" "public_route" {
   vpc_id = aws_vpc.max_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.max-gw.id
+  }
+}
+
+resource "aws_route_table" "private_route" {
+  vpc_id = aws_vpc.max_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gateway.id
   }
 }
 
